b323767c8fb9c1d754c3f410e8f4426e962b77d4
keelim <kimh00335@gmail.com>
2018-10-12 19:24:38 +0900

    Update toy1/LoginFrame.java

--- toy1/LoginFrame.java
+++ toy1/LoginFrame.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
