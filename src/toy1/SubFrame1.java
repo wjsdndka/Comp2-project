43822c540085ca0f736367a2e4d1e9e1518beff2
keelim <kimh00335@gmail.com>
2018-10-13 03:02:33 +0900

    2018 10 13 2
    2018 10 13 2

---
+++ src/toy1/SubFrame1.java
@@ -0,0 +1,13 @@
+package toy1;
+
+import javax.swing.JFrame;
+
+public class SubFrame1 extends JFrame{
+	public SubFrame1() {
+		setTitle("서브 프레임 입니다.");
+		setSize(500, 500);
+		
+		setVisible(true);
+		
+	}
+}
