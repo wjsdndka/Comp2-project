a54dbb847b0c62512e87399aaf2481d07e316a65
keelim <kimh00335@gmail.com>
2018-10-13 03:01:06 +0900

    2018 1013 1
    20181013 1

--- README.md
+++
@@ -1,5 +0,0 @@
-###
-2018 10 12 
-Computer 2 project start 
-make 
-
--- toy1/Main.java
+++
@@ -1,9 +0,0 @@
-package toy1;
-
-public class Main {
-	public static void main(String[] args) {
-		new LoginFrame();
-	}
-		
-	
-}
