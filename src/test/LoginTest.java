0305bc227b8a0336c27703337e92ad4a78e6d6fb
keelim/CP2_201503069 <kimh00335@gmail.com>
2018-11-05 15:51:57 +0900

    DB update
    DB update

---
+++ src/test/LoginTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import assets.DBConnectionMgr;
+
+public class LoginTest {
+	public static void main(String[] args) {
+		
+	}
+	public static boolean loginTest(String id, String password) {
+		boolean flag = flase;
+		
+		DBConnectionMgr pool = DBConnectionMgr.getInstance();
+		
+	}
+}
